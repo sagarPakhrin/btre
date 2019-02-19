0b3c44cc598b04619d4d10ec458768dd95074e3e
Sagar Lama <sagarlama686@gmail.com>
2019-02-19 19:45:52 +0545

    list display changed

--- listings/admin.py
+++ listings/admin.py
@@ -1,5 +1,12 @@
 from django.contrib import admin
 from listings.models import Listing
 # Register your models here.
+class ListingAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'is_published','price', 'list_date','realtor')
+    # this allows us to click on the title or the id to go to the details of the listings
+    list_display_links = ('id','title')
+    # this helps us to filter listings based on realtor
+    list_filter = ('realtor',)
+    list_editable = ('is_published',)
+admin.site.register(Listing, ListingAdmin)
 
-admin.site.register(Listing)
